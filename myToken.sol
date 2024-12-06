14a9e2e1056108fdd2140914d6ffcfa15226c197
Ywan Scazi Galura <163106577+OshiBoii@users.noreply.github.com>
2024-12-06 15:24:05 +0800

    Update myToken.sol

--- myToken.sol
+++ myToken.sol
@@ -4,25 +4,30 @@ pragma solidity ^0.8.0;
 // Importing OpenZeppelin contracts from CDN
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-// Silver Token (SLVR)
-// Only contract owner should be able to mint tokens
-// Any user can transfer tokens
-// Any user can burn tokens
-
-contract Silver is ERC20, ERC20Burnable, Ownable {
+contract Silver is ERC20, Ownable {
 
     // The constructor for the Silver token contract
     constructor(address initialOwner) 
         ERC20("Silver", "SLVR")  // Call the ERC20 constructor with token name and symbol
-        Ownable(initialOwner)  // Call the Ownable constructor to set the deployer as the owner
+        Ownable(initialOwner)    // Pass the initial owner to the Ownable constructor
     {
-        // The deployer's address is automatically set as the owner by Ownable constructor
+        // Initialization logic, if needed
     }
 
     // Mint function (only accessible by the contract owner)
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    // Burn function (open to any token holder)
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    // Transfer function (overrides default ERC20 transfer for explicit definition)
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
     }
 }
